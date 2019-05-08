270ed7ee71a67a06918561443b13c4079898acdd
Valcinei Silva <valcinei.silva@fpf.br>
2019-05-08 10:33:52 -0400

    fix feedbacks

--- src/commands/create-restify.ts
+++ src/commands/create-restify.ts
@@ -20,43 +20,43 @@ module.exports = {
       template: 'index.ts.ejs',
       target: `${name}/src/index.js`,
       props: { name },
-    })
+    }).then((res)=> success("Generated index.js"))
 
     await generate({
       template: 'server.ts.ejs',
       target: `${name}/src/server.js`,
       props: { name },
-    })
+    }).then((res)=> success("Generated server.js"))
 
     await generate({
       template: 'routes.ts.ejs',
       target: `${name}/src/routes.js`,
       props: { name },
-    })
+    }).then((res)=> success("Generated routes.js"))
 
     await generate({
       template: 'controller.ts.ejs',
       target: `${name}/src/Http/Controllers/UserController.js`,
       props: { name },
-    })
+    }).then((res)=> success("Generated controller"))
 
     await generate({
       template: 'model.ts.ejs',
       target: `${name}/src/Models/User.js`,
       props: { name },
-    })
+    }).then((res)=> success("Generated model"))
   
     await generate({
       template: 'package.json.ejs',
       target: `${name}/package.json`,
       props: { name },
-    })
+    }).then((res)=> success("Generated package.json"))
     
     info('Installing dependencies...')
     toolbox.system.run(`npm --prefix ${name} install`, { trim: true })
     .then((res)=>{
       success(res)
-      success(`App ${name} generated sucess`)
+      success(`App Restify ${name} generated sucess`)
       warning(`cd ${name}`)
       warning(`npm run dev`)
     })
